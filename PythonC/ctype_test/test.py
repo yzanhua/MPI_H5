b1b034b79f2fb724956dd7b77d7f853883d29815
zanhua huang <yzanhua@gmail.com>
2021-09-28 13:12:21 -0500

    code: ctypes test

--- PythonC/ctype_test/test.py
+++ PythonC/ctype_test/test.py
@@ -1,7 +1,46 @@
 import ctypes
+import time
+import numpy as np
 
+
+# create data
+NUM=1000000+1
+data = np.array([i for i in range(NUM)])
+
+# load c_modules
 spam = ctypes.CDLL("./spam.so")
 
-li = (ctypes.c_int * 5)(1, 2, 3, 4, 5)
-a = spam.sum(li, len(li))
-print(a)
+# convert data to ctypes-readable format
+c_list = (ctypes.c_int * NUM)(*data)
+
+# sum - by ctypes
+start = time.time()
+c_sum = spam.sum(c_list, len(c_list))
+end = time.time()
+c_time = start - end
+
+
+# sum - by python built in
+start = time.time()
+builtin_sum = sum(data)
+end = time.time()
+builtin_time = start - end
+
+# sum - by numpy
+start = time.time()
+np_sum = np.sum(data)
+end = time.time()
+np_time = start - end
+
+# sum - python loop
+pyloop_sum = 0
+start = time.time()
+for x in data:
+    pyloop_sum += x
+end = time.time()
+pyloop_time = start - end
+
+print(c_sum, c_time)
+print(builtin_sum, builtin_time)
+print(np_sum, np_time)
+print(pyloop_sum, pyloop_time)
