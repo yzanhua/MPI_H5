d18bae91a3046261820dcda472171e0637974075
zanhua huang <yzanhua@gmail.com>
2021-09-28 14:09:06 -0500

    code: hello cpp

---
+++ mpi_c_cpp/HelloCPP/hello_cpp.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <mpi.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
+    // Print off a hello world message
+    cout << "Hello world from processor " << processor_name << " rank " << world_rank << " out of " << world_size << " processors" << endl;
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+
+    vector<int> test(5);
+    cout << test.size() << endl;
+}
