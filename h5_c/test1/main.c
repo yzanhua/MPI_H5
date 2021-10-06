75204860ad76a87c680c68c9733cad75bda51173
zanhua huang <yzanhua@gmail.com>
2021-10-05 23:44:18 -0500

    code: add c code to read a data set

--- h5_c/test1/main.c
+++ h5_c/test1/main.c
@@ -7,38 +7,46 @@
  ************************************************************/
 
 #include "hdf5.h"
+#include "hdf5_hl.h"
+#include "stdio.h"
+#include "stdlib.h"
 
-#define FILE "h5ex_g_create.h5"
+#define FILE "../h5files/py_create1.h5"
 
-int
-main(void)
+int main(void)
 {
-    hid_t       file, group;         /* Handles */
-    herr_t      status;
+  hid_t file, dset, dcpl; /* Handles */
+  herr_t status;
+  
+  
+  // H5Eset_auto(H5E_DEFAULT, NULL, NULL);
+  file = H5Fopen(FILE, H5F_ACC_RDONLY, H5P_DEFAULT);
+  dset = H5Dopen(file, "numbers", H5P_DEFAULT);
+  hid_t dspace = H5Dget_space(dset);
 
-    /*
-     * Create a new file using the default properties.
-     */
-    file = H5Fcreate (FILE, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+  // get ndims:
+  // const int ndims = H5Sget_simple_extent_ndims(dspace);
+  // hsize_t dims[ndims];
+  // H5Sget_simple_extent_dims(dspace, dims, NULL);
 
-    /*
-     * Create a group named "G1" in the file.
-     */
-    group = H5Gcreate (file, "/G1", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+  // get size // H5Dget_type(dset)
+  hsize_t dsize = H5Sget_simple_extent_npoints(dspace) * H5Tget_size(H5Dget_type(dset));
 
-    /*
-     * Close the group.  The handle "group" can no longer be used.
-     */
-    status = H5Gclose (group);
+  void* buff = malloc(dsize);
 
-    /*
-     * Re-open the group, obtaining a new handle.
-     */
-    group = H5Gopen (file, "/G1", H5P_DEFAULT);
+  status = H5Dread(dset, H5Dget_type(dset), H5S_ALL, H5S_ALL, H5P_DEFAULT, buff);
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < 10; j++)
+    {
+      // printf("%ld ", *((long*)(buff + (i * 10 + j) * H5Tget_size(H5Dget_type(dset))) ));
+      printf("%d ", ((int*)buff)[i * 10 + j]);
+    }
+    printf("\n");
+  }
 
-    /*
-     * Close and release resources.
-     */
-    status = H5Gclose (group);
-    status = H5Fclose (file);
+  free(buff);
+  status = H5Sclose(dspace);
+  status = H5Dclose(dset);
+  status = H5Fclose(file);
 }
